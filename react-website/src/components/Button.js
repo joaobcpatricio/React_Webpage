931fc2efcfbd7280ab48111e26dd5194af5f6800
joaobcpatricio <castanheirapatricio@outlook.com>
2021-02-13 12:44:07 +0100

    Update css files on design

--- react-website/src/components/Button.js
+++ react-website/src/components/Button.js
@@ -14,9 +14,7 @@ export const Button = ({
     buttonStyle, 
     buttonSize
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)    
-    ? buttonStyle 
-    : STYLES[0];
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
